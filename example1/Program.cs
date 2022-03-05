47e7d43cfb861472d72d0a8cc470047f59b59022
Anna <adjikiss@mail.ru>
2022-03-05 18:05:47 +0300

    Example1 finally hopefilly added

---
+++ example1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа принимает на вход координаты точки (причем х и у не равны нулю),
+// Выдает на выходе номер четверти плоскости, в которой находится точка.
+int GetQuart(double xPoint, double yPoint)
+{
+    if (xPoint > 0 && yPoint > 0) return 1;
+    if (xPoint < 0 && yPoint > 0) return 2;
+    if (xPoint < 0 && yPoint < 0) return 3;
+    if (xPoint > 0 && yPoint < 0) return 4;
+    return 0;                               
+}
+double x,y;
+int quartNum;
+Console.Write ("Input x:  ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Input y: ");
+y = Convert.ToDouble(Console.ReadLine());
+quartNum = GetQuart(x, y);
+if (quartNum == 0) Console.WriteLine("It's impossible to determine quarter");
+else Console.WriteLine("The point is in " + quartNum + " quarter");
